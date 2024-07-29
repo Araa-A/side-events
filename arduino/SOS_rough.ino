446521fa6e1b9b11f4e73e8b51d2e1b7e7e119d2
Araa Asmah Mohd Nasrizal <75965324+Araa-A@users.noreply.github.com>
2024-07-29 14:21:58 +0800

    Create SOS_rough.ino

---
+++ arduino/SOS_rough.ino
@@ -0,0 +1,52 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(13,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //S
+digitalWrite(13,HIGH);
+delay(50);
+digitalWrite(13,LOW);
+delay(50);
+digitalWrite(13,HIGH);
+delay(50);
+digitalWrite(13,LOW);
+delay(50);
+digitalWrite(13,HIGH);
+delay(50);
+digitalWrite(13,LOW);
+delay(50);
+// O
+digitalWrite(13,HIGH);
+delay(500);
+digitalWrite(13,LOW);
+delay(500);
+digitalWrite(13,HIGH);
+delay(500);
+digitalWrite(13,LOW);
+delay(500);
+digitalWrite(13,HIGH);
+delay(500);
+digitalWrite(13,LOW);
+delay(500);
+
+//S
+digitalWrite(13,HIGH);
+delay(50);
+digitalWrite(13,LOW);
+delay(50);
+digitalWrite(13,HIGH);
+delay(50);
+digitalWrite(13,LOW);
+delay(50);
+digitalWrite(13,HIGH);
+delay(50);
+digitalWrite(13,LOW);
+delay(50);
+
+//Delay
+
+delay(1000);
+}
