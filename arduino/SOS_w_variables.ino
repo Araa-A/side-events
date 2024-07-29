bd01a759c5be0f896421bd10a32c7ca7648397c2
Araa Asmah Mohd Nasrizal <75965324+Araa-A@users.noreply.github.com>
2024-07-29 14:32:33 +0800

    Create SOS_w_variables.ino

---
+++ arduino/SOS_w_variables.ino
@@ -0,0 +1,57 @@
+int ledPin=8;
+int dit=100;
+int dah=500;
+int longW=1000;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(ledPin,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //S
+digitalWrite(ledPin,HIGH);
+delay(dit);
+digitalWrite(ledPin,LOW);
+delay(dit);
+digitalWrite(ledPin,HIGH);
+delay(dit);
+digitalWrite(ledPin,LOW);
+delay(dit);
+digitalWrite(ledPin,HIGH);
+delay(dit);
+digitalWrite(ledPin,LOW);
+delay(dit);
+// O
+digitalWrite(ledPin,HIGH);
+delay(dah);
+digitalWrite(ledPin,LOW);
+delay(dah);
+digitalWrite(ledPin,HIGH);
+delay(dah);
+digitalWrite(ledPin,LOW);
+delay(dah);
+digitalWrite(ledPin,HIGH);
+delay(dah);
+digitalWrite(13,LOW);
+delay(dah);
+
+//S
+digitalWrite(ledPin,HIGH);
+delay(dit);
+digitalWrite(ledPin,LOW);
+delay(dit);
+digitalWrite(ledPin,HIGH);
+delay(dit);
+digitalWrite(ledPin,LOW);
+delay(dit);
+digitalWrite(ledPin,HIGH);
+delay(dit);
+digitalWrite(ledPin,LOW);
+delay(dit);
+
+//Delay
+
+delay(longW);
+}
