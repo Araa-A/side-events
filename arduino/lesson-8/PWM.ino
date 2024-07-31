15773aaca4faaf7f05d6d85eb69d6870312d8b2c
Araa Asmah Mohd Nasrizal <75965324+Araa-A@users.noreply.github.com>
2024-07-31 15:22:57 +0800

    Create PWM.ino

---
+++ arduino/lesson-8/PWM.ino
@@ -0,0 +1,12 @@
+
+// analogWrite("pinNumber","value between 0-255");  the value 0-255 corresponds to 0-5 volts
+
+//this command uses - pulse width modulation: 
+// this means that the Volts are only either 0 or 5 
+// but pulse on and off at a speed that is dependent on what value you are using.
+
+
+// how to view it: imagine a person is running at a speed of 5mph, 
+// for 0.1 seconds before they stop for 0.1 seconds. 
+// from your perspective it would appear that the person is running at a speed of 2.5mph 
+// even though they are actually only ever running at a speed of either 5mph or 0mph
